cc245c36abb4510e4b8c3f8c76cc9ac1583472d9
Bruno Lindoso <48108317+bslindoso@users.noreply.github.com>
2024-12-29 09:59:00 -0300

    29-12-2024 001

---
+++ 5. Consolide Seu Conhecimento com Desafios de Código JavaScript/maiorParMenorImpar.js
@@ -0,0 +1,22 @@
+const { gets, print, not } = require('../!Aplicativos/utils')
+
+const lista = gets('intlist', 10)
+print('================ NÚMEROS ===============')
+print(lista)
+
+let maior_par = lista.filter(n => isPar(n))[0]
+let menor_impar = lista.filter(n => not(isPar(n)))[0]
+
+lista.forEach(n => {
+  if (maior_par < n && isPar(n)) maior_par = n
+  if (menor_impar > n && not(isPar(n))) menor_impar = n
+});
+
+function isPar(n) {
+  return n % 2 === 0
+}
+
+print('=============== NÚMEROS PARES ===============')
+print('Maior par', maior_par)
+print('=============== NÚMEROS ÍMPARES ===============')
+print('Menor ímpar', menor_impar)
---
+++ 5. Consolide Seu Conhecimento com Desafios de Código JavaScript/media.js
@@ -0,0 +1,27 @@
+
+/**
+ * @fileoverview Programa para avaliação de média escolar
+ * @requires ../!Aplicativos/utils
+ * 
+ * @description
+ * Este programa realiza:
+ * - Classificação do aluno baseado na média:
+ *   - Média >= 7: Aprovado
+ *   - 5 <= Média < 7: Recuperação
+ *   - Média < 5: Reprovado
+ * 
+ */
+
+const { gets, print } = require('../!Aplicativos/utils')
+
+const media = gets('float', 10)
+const aluno = gets('name')
+print(`Média do(a) aluno(a) ${aluno}`, media)
+
+if (media >= 7) {
+  print('Aprovado')
+} else if (media < 7 && media >= 5) {
+  print('Recuperação')
+} else if (media < 5) {
+  print('Reprovado')
+}
