6fc9ce214202c7ba48ba71a3612b368f4348206c
Bruno Lindoso <48108317+bslindoso@users.noreply.github.com>
2025-01-13 21:43:22 -0300

    13-01-2025 typescript

---
+++ dio_formacao_nodeJs_fundamentals/11-typescript/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// src/index.ts
+function main() {
+  let myEmail = "bacaca@c.com";
+  console.log(myEmail);
+}
+main();
---
+++ dio_formacao_nodeJs_fundamentals/11-typescript/src/index.ts
@@ -0,0 +1,6 @@
+function main() {
+  let myEmail: string = "bacaca@c.com"
+  console.log(myEmail)
+}
+
+main()
